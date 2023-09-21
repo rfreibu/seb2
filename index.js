7ef35a2f21daaaac7b5fd18b70bfd3ceb8de7443
rfreibu <35344377+rfreibu@users.noreply.github.com>
2023-09-20 21:11:39 -0500

    aaaa

---
+++ index.js
@@ -0,0 +1,30 @@
+var slideIndex= 1;
+showSlide(slideIndex);
+
+function moveSlide(n) {
+    showSlide(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlide(slideIndex = n);
+}
+
+function showSlide(n) {
+    var i;
+    var slides = document.getElementsByClassName("slideshow");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+var slideInterval = setInterval(function(){
+    moveSlide(1);
+}, 5000); // Change image every 5 seconds
